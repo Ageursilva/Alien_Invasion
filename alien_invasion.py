74a276a7e4bdc19c1ac0e0750e97d89f2ebecc8c
Ageu Silva <ageu.tecnologia@gmail.com>
2020-06-24 22:04:47 -0300

    Adicionado settings e a ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,14 @@ import sys
 
 import pygame
 
+from settings import Settings
 
 def run_game():
-    #Inicializa o jogo e cria um objeto para a tela
+    #Inicializa o  pygame, as configurações e objeto screen
     pygame.init()
-    screen = pygame.display.set_mode((600, 600))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #Inicia o laço princiapl do jogo
@@ -16,7 +19,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        
+        #redesenha a tela a cada passagem pelo laço
+        screen.fill(ai_settings.bg_color)
+    
          #Deixa a tela mais recente visivel
         pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """ Uma classe para armazenar todas as configurações da Invasão Alien"""
+
+    def __init__(self):
+        """Inicializa as configuraçoões do jogo."""
+        #configurações da tela
+        self.screen_width = 600
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
